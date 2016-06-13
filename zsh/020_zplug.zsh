606fe0e916b7a71006077ef8900ad7aaa670ce3c
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2016-06-13 07:41:22 +0000

    ファイルが存在する場合に処理するようにスクリプトを変更。
    On branch feature/1
    Your branch is up-to-date with 'origin/feature/1'.
    
    Changes to be committed:
    
      - modified:   zsh/020_zplug.zsh

--- zsh/020_zplug.zsh
+++ zsh/020_zplug.zsh
@@ -1,15 +1,17 @@
 #
 # zsh/020_zplug.zsh
 #
-# 設定ファイルが存在しなければ何もしない
-[ ! -f ~/.zplug/init.zsh ] && exit 1
-source ~/.zplug/init.zsh
-zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    echo; zplug install
-fi
-# Then, source plugins and add commands to $PATH
-zplug load --verbose
+# 設定ファイルが存在する場合のみ実行する
+if [ -f ~/.zplug/init.zsh ]; then
+  source ~/.zplug/init.zsh
+  zplug "zsh-users/zsh-history-substring-search"
+  zplug "zsh-users/zsh-syntax-highlighting", nice:10
 
+  # Install plugins if there are plugins that have not been installed
+  if ! zplug check --verbose; then
+    echo; zplug install
+  fi
+
+  # Then, source plugins and add commands to $PATH
+  zplug load --verbose
+fi
