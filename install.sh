9ebb451002b04d04a104c150bbd048752f142817
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2016-07-10 14:48:58 +0900

    refs feature/10 fzf のインストール機能を実装
    環境に fzf がインストールされていない場合に Github からソースコードを `clone` してインストールする機能を実装。
    注意点は以下の通り。
    
      - 環境に `git` がインストールされていること
      - fzf のインストールスクリプトは対話式になっているが、その部分は自動化されていない
    
    ----
    
    On branch feature/10
    Changes to be committed:
    
      - new file:   etc/install/040_install_fzf
      - modified:   install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ cd `dirname $0`
 # https://github.com/zplug/zplug
 #curl -sL get.zplug.sh | zsh
 
+# dotfiles/etc/install にあるスクリプトを実行する
+INSTALL_SCRIPT_DIR=etc/install
+for F in `ls $INSTALL_SCRIPT_DIR`
+do
+    sh $INSTALL_SCRIPT_DIR/$F
+done
+
 # dotfiles/etc/deploy にあるスクリプトを実行する
 DEPLOY_SCRIPT_DIR=etc/deploy
 for F in `ls $DEPLOY_SCRIPT_DIR`
