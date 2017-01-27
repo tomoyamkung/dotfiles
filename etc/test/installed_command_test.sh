7729fe347b0de675bd7e2a8260f2d212fffd2442
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-01-27 19:58:54 +0900

    refs feature/49 #49 テストケースを修正
    紛らわしいテストケースを削除。
    
    ----
    
    On branch feature/49
    Changes to be committed:
      - modified:   etc/test/installed_command_test.sh

--- etc/test/installed_command_test.sh
+++ etc/test/installed_command_test.sh
@@ -6,18 +6,5 @@ cd `dirname $0`
 # テスト対象のスクリプトを読み込む
 . ../lib/installed_command.sh
 
-# `ls` コマンドは存在するので `$? = 0` になるはず
-installed_command ls 
-if [ ! $? -eq 0 ]; then
-    echo "ls コマンドは存在します"
-fi
-
-# `dummy_command` コマンドは存在しないので `$? != 0` になるはず
-installed_command dummy_command 
-if [ $? -eq 0 ]; then
-    echo "dummy_command コマンドは存在しません"
-fi
-
-# 別解
 installed_command ls || echo "ls はインストールされているはずです"
 installed_command dummy_command && echo "dummy_command はインストールされていないはずです"
