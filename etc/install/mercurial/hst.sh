d0e82224ae8a88465719b45c3eeadc1c25b58613
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-01-29 08:34:30 +0000

    refs feature/42 #42 絞り込んだファイルが存在しなかった場合の対応を実装
    FZF で絞り込んだファイルが存在しなかった場合の対応を実装したのでコミット。
    
    ----
    
    On branch feature/42
    Changes to be committed:
      - modified:   etc/install/mercurial/hst.sh

--- etc/install/mercurial/hst.sh
+++ etc/install/mercurial/hst.sh
@@ -48,8 +48,13 @@ if [ "$command" == "" ]; then
 fi
 
 # それ以外の場合は FZF で対象となるファイルを絞り込んで `hg add/diff/revert` を実行する
-# fzf --exit-0: 絞り込む要素が存在しない場合に fzf インタフェースを立ち上げずに終了する
-# fzf --select-1: 絞り込む要素が１つしか存在しない場合に fzf インタフェースを立ち上げずに終了する
-# fzf --ansi: 絞り込みを色付けして表示する
-hg branches | fzf --exit-0 --select-1 --ansi | awk '{print $1}' | xargs hg "$command"
+#   fzf --exit-0: 絞り込む要素が存在しない場合に fzf インタフェースを立ち上げずに終了する
+#   fzf --select-1: 絞り込む要素が１つしか存在しない場合に fzf インタフェースを立ち上げずに終了する
+#   fzf --ansi: 絞り込みを色付けして表示する
+file_name=$(hg st | fzf --exit-0 --select-1 --ansi)
+# 存在しないファイルを指定された場合は処理を終了する
+[[ -z ${file_name} ]] && exit 2
+
+file_name=$(echo ${file_name} | awk '{print $2}')
+hg ${command} ${file_name}
 exit 0
