de63d3ab51420abd61b87112acb3d59420e0083b
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2017-01-29 15:04:06 +0000

    refs feature/42 #42 dry-run モードを実装
    x オプションにより dry-run で実行する仕組みを実装したのでコミット。
    
    ----
    
    On branch feature/42
    Changes to be committed:
      - modified:   etc/install/mercurial/hst.sh

--- etc/install/mercurial/hst.sh
+++ etc/install/mercurial/hst.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -eu
 
+# ライブラリスクリプトを読み込む
+. ~/dotfiles/etc/lib/dry_run.sh
+
 function usage() {
   cat <<EOF 1>&2
 Description:
   $(basename ${0}) は以下を実行するスクリプトである（カッコ書きは指定するオプション）。
     1. プロジェクトの変更を確認する（オプションなし）
-	2. Mercurial の管理下にファイルを追加する（a オプション）
-	3. ファイルの変更を確認する（d オプション）
-	4. ファイルの変更を戻す（r オプション）
+    2. Mercurial の管理下にファイルを追加する（a オプション）
+    3. ファイルの変更を確認する（d オプション）
+    4. ファイルの変更を戻す（r オプション）
   オプションを指定しない場合は "hg st" を実行する。
   オプションを指定した場合はいずれも "hg st" した結果を FZF によりファイルを絞り込む仕組みになっている。
 
@@ -27,29 +30,31 @@ EOF
 # `hg` のコマンドを格納する変数
 # スクリプトのオプションで指定する
 command=""
-while getopts adrh OPT
+while getopts adrhx OPT
 do
-    case "$OPT" in
-        a) command="add"
-            ;;
-        d) command="diff"
-            ;;
-        r) command="revert"
-            ;;
-        h) usage
-            ;;
-        \?) usage
-            ;;
+  case "$OPT" in
+    a) command="add"
+       ;;
+    d) command="diff"
+       ;;
+    r) command="revert"
+       ;;
+    h) usage
+       ;;
+    x) enable_dryrun
+       ;;
+    \?) usage
+      ;;
     esac
 done
 
 # 変数 $command がブランクの場合は `hg st` を実行する
 if [ "$command" == "" ]; then
-    hg st
-    exit 0
+  ${dryrun} hg st
+  exit 0
 fi
 
-# それ以外の場合は FZF で対象となるファイルを絞り込んで `hg add/diff/revert` を実行する
+# それ以外の場合は FZF で対象となるファイルを絞り込んで `hg add or diff or revert` を実行する
 #   fzf --exit-0: 絞り込む要素が存在しない場合に fzf インタフェースを立ち上げずに終了する
 #   fzf --select-1: 絞り込む要素が１つしか存在しない場合に fzf インタフェースを立ち上げずに終了する
 #   fzf --ansi: 絞り込みを色付けして表示する
@@ -58,5 +63,5 @@ file_name=$(hg st | fzf --exit-0 --select-1 --ansi)
 [[ -z ${file_name} ]] && exit 2
 
 file_name=$(echo ${file_name} | awk '{print $2}')
-hg ${command} ${file_name}
+${dryrun} hg ${command} ${file_name}
 exit 0
